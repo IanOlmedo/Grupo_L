00025de3df24ef263aa02d935b702f2b0dd6cc58
Martin-NT <martinnavarro1503@gmail.com>
2024-09-22 11:30:45 -0300

    angular

--- frontend/src/src/app/app.module.ts
+++ frontend/src/src/app/app.module.ts
@@ -7,6 +7,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 import { VerLibroComponent } from './pages/ver-libro/ver-libro.component';
 import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { AbmComponent } from './components/abm/abm.component';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
+import { PrestamosComponent } from './pages/prestamos/prestamos.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { AcercaDeComponent } from './pages/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +22,15 @@ import { CatalogoComponent } from './pages/catalogo/catalogo.component';
     HomeComponent,
     NavComponent,
     VerLibroComponent,
-    CatalogoComponent
+    CatalogoComponent,
+    SearchBarComponent,
+    AbmComponent,
+    UsuariosComponent,
+    PrestamosComponent,
+    LoginComponent,
+    ErrorPageComponent,
+    RegisterComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
