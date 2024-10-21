ec8e34ec0428422e3dfc6a708d147c8f0123439a
IanOlmedo <i.olmedo@alumno.um.edu.ar>
2024-10-21 17:34:30 -0300

    prestamos component/page

--- frontend/src/src/app/components/pres-admin/pres-admin.component.ts
+++ frontend/src/src/app/components/pres-admin/pres-admin.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pres-admin',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class PresAdminComponent {
 
+
+
+  constructor(
+    private route: ActivatedRoute
+  ){}
+
+  get isToken() {
+    return localStorage.getItem('token');
+  }  
+
+  get isRole() {
+    return localStorage.getItem('user_role');
+  }  
+
+  get isUser() {
+    return this.isRole == 'user';
+  }
+
+  get isAdmin() {
+    return this.isRole == 'admin';
+  }
 }
+
+
+
