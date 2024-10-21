66c320a655df342802631f5d1e14e4caa308e8e3
Reynier124 <reynier1306@gmail.com>
2024-10-21 20:20:50 -0300

    ver_libro

--- frontend/src/src/app/pages/ver-libro/ver-libro.component.ts
+++ frontend/src/src/app/pages/ver-libro/ver-libro.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {BooksService} from './../../services/books.service'
 
 @Component({
   selector: 'app-ver-libro',
@@ -9,13 +10,39 @@ import { ActivatedRoute } from '@angular/router';
 export class VerLibroComponent {
   var_id!:string
   var_rol!:string
+
+  book:any
+  arrayAuthor:any[] = []
+
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private booksService: BooksService,
   ){}
+
 
   ngOnInit(){
     this.var_id = this.route.snapshot.paramMap.get('id') || '';
-    this.var_rol=this.route.snapshot.paramMap.get('rol') || '';
+    this.booksService.getOneBook(this.var_id).subscribe((rta:any) =>{
+      console.log(rta)
+      this.book = rta || null
+      console.log(this.book)
+      console.log(this.book.año_de_publicacion)
+      this.arrayAuthor = rta.autor || []
+      console.log(this.arrayAuthor)
+    },
+  )
   }
-  
+/*
+  ngOnInit(){
+    this.var_id = this.route.snapshot.paramMap.get('id') || '';
+    this.book = this.booksService.getOneBook(this.var_id);
+    console.log(this.book)
+    console.log(this.book.titulo)
+    this.book.subscribe((b:any) => {
+      this.autor = b.autor;
+      console.log(this.autor)
+      console.log(this.autor.id_autor)
+    });
+  }
+  */
 }
