2b11568b522e9e2a0a66df94389f434df875fc79
Martin-NT <martinnavarro1503@gmail.com>
2024-10-21 22:48:54 -0300

    Valoraciones modificacion

--- frontend/src/src/app/pages/valoraciones/valoraciones.component.ts
+++ frontend/src/src/app/pages/valoraciones/valoraciones.component.ts
@@ -10,11 +10,14 @@ export class ValoracionesComponent {
   @Input() var_id!: string;
 
   cartItems = [
-    { usuario: 'Martin Navarro', avatar: 'assets/Martin.jpg', calificacion: 5, titulo: 'Fuego y Sangre', portada: "assets/Fuego_y_Sangre.jpeg", valoracion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit volutpat  tincidunt velit, diam molestie tortor metus dui vestibulum mollis  viverra placerat vestibulum mollis  viverra placerat'},
-    { usuario: 'Ian Olmedo', avatar: 'assets/ian.jpeg', calificacion: 1, titulo: 'Fuego y Sangre', portada: "assets/Fuego_y_Sangre.jpeg", valoracion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit volutpat  tincidunt velit, diam molestie tortor metus dui vestibulum mollis  viverra placerat vestibulum mollis  viverra placerat'},
-    { usuario: 'Camila Choque', avatar: 'assets/cami.jpeg', calificacion: 3, titulo: 'Fuego y Sangre', portada: "assets/Fuego_y_Sangre.jpeg", valoracion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit volutpat  tincidunt velit, diam molestie tortor metus dui vestibulum mollis  viverra placerat vestibulum mollis  viverra placerat'},
-    { usuario: 'Luciano Toneatti', avatar: 'assets/luchi.jpeg', calificacion: 4, titulo: 'Fuego y Sangre', portada: "assets/Fuego_y_Sangre.jpeg", valoracion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit volutpat  tincidunt velit, diam molestie tortor metus dui vestibulum mollis  viverra placerat vestibulum mollis  viverra placerat'},
-    { usuario: 'Reynier Lopez', avatar: 'assets/reynier.jpeg', calificacion: 2, titulo: 'Fuego y Sangre', portada: "assets/Fuego_y_Sangre.jpeg", valoracion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit volutpat  tincidunt velit, diam molestie tortor metus dui vestibulum mollis  viverra placerat vestibulum mollis  viverra placerat'},
+    { usuario: 'Martin Navarro', avatar: 'assets/Martin.jpg', calificacion: "5 estrellas" , titulo: 'Fuego y Sangre', portada: "assets/Fuego_y_Sangre.jpeg", valoracion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit volutpat  tincidunt velit, diam molestie tortor metus dui vestibulum mollis  viverra placerat vestibulum mollis  viverra placerat'},
+    { usuario: 'Ian Olmedo', avatar: 'assets/ian.jpeg', calificacion: "1 estrella", titulo: 'Fuego y Sangre', portada: "assets/Fuego_y_Sangre.jpeg", valoracion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit volutpat  tincidunt velit, diam molestie tortor metus dui vestibulum mollis  viverra placerat vestibulum mollis  viverra placerat'},
+    { usuario: 'Camila Choque', avatar: 'assets/cami.jpeg', calificacion: "3 estrellas", titulo: 'Fuego y Sangre', portada: "assets/Fuego_y_Sangre.jpeg", valoracion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit volutpat  tincidunt velit, diam molestie tortor metus dui vestibulum mollis  viverra placerat vestibulum mollis  viverra placerat'},
+    { usuario: 'Luciano Toneatti', avatar: 'assets/luchi.jpeg', calificacion: "4 estrellas", titulo: 'Fuego y Sangre', portada: "assets/Fuego_y_Sangre.jpeg", valoracion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit volutpat  tincidunt velit, diam molestie tortor metus dui vestibulum mollis  viverra placerat vestibulum mollis  viverra placerat'},
+    { usuario: 'Reynier Lopez', avatar: 'assets/reynier.jpeg', calificacion: "2 estrellas", titulo: 'Fuego y Sangre', portada: "assets/Fuego_y_Sangre.jpeg", valoracion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit volutpat  tincidunt velit, diam molestie tortor metus dui vestibulum mollis  viverra placerat vestibulum mollis  viverra placerat'},
   ];  
 
+  eliminarValoracion(index: number) {
+    this.cartItems.splice(index, 1); // Elimina el elemento de la posición indicada
+  }
 }
